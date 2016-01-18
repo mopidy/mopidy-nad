663c4fc3a19938bf5039c4b9fc0526a397c5f7e5
Stein Magnus Jodal <stein.magnus@jodal.no>
2016-01-18 20:30:01 +0100

    Switch from nosetests to pytest

--- setup.py
+++ setup.py
@@ -29,11 +29,6 @@ setup(
         'Pykka >= 1.1',
         'pyserial',
     ],
-    test_suite='nose.collector',
-    tests_require=[
-        'nose',
-        'mock >= 1.0',
-    ],
     entry_points={
         'mopidy.ext': [
             'nad = mopidy_nad:Extension',
